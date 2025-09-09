a04e7da025ef319573db45686358bb4662bc0cd9
Mohammed Badawy <121061414+Mohamedbadawyy12@users.noreply.github.com>
2025-09-09 15:08:32 +0300

    Delete duplicate main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI 
-from dotenv import load_dotenv #before the base read it
-load_dotenv(".env")
-
-from routes import base #base.py
-
-
-app=FastAPI()
-app.include_router(base.base_router)
